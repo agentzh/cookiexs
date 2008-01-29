1b641e71e00ac30d3d868e6551f6514c5a9fb20a
agentz <agentz@1b84fc59-20ce-dc11-bd8a-000bcdefe56b>
2008-01-29 04:15:35 +0000

    r7250@agentz-office (orig r1644):  agentzh | 2008-01-29 12:01:12 +0800  [cookie-parser] added lib/Cookie/Parser
    git-svn-id: http://svn.openfoundry.org/cookieparser@2 1b84fc59-20ce-dc11-bd8a-000bcdefe56b

---
+++ lib/Cookie/Parser.pm
@@ -0,0 +1,29 @@
+package Cookie::Parser;
+
+use strict;
+use warnings;
+
+use FindBin;
+use Data::Dumper;
+#our $VERSION = '0.04';
+
+use XSLoader;
+BEGIN {
+    XSLoader::load(__PACKAGE__);
+}
+
+mkdir '/tmp' if !-d '/tmp';
+mkdir '/tmp/_Inline' if !-d '/tmp/_Inline';
+
+sub fetch {
+    my $class = shift;
+    my $raw_cookie = $ENV{HTTP_COOKIE} || $ENV{COOKIE} or return;
+    $class->parse($raw_cookie);
+}
+
+sub parse {
+    parse_cookie($_[1]);
+}
+
+1;
+
