3339598ac78d5e19993ce0ec70d76c25d7c3f2b3
agentz <agentz@1b84fc59-20ce-dc11-bd8a-000bcdefe56b>
2008-03-24 02:23:06 +0000

    r8115@agentz-office (orig r2052):  agentzh | 2008-03-24 10:18:01 +0800  [cookie-xs] CPAN release 0.09
    git-svn-id: http://svn.openfoundry.org/cookieparser@35 1b84fc59-20ce-dc11-bd8a-000bcdefe56b

--- lib/Cookie/XS.pm
+++ lib/Cookie/XS.pm
@@ -7,7 +7,7 @@ our $VERSION;
 
 use XSLoader;
 BEGIN {
-    $VERSION = '0.08';
+    $VERSION = '0.09';
     XSLoader::load(__PACKAGE__, $VERSION);
 }
 
@@ -30,7 +30,7 @@ Cookie::XS - HTTP Cookie parser in C
 
 =head1 VERSION
 
-This document describes Cookie::XS 0.08 released on March 20, 2008.
+This document describes Cookie::XS 0.09 released on March 24, 2008.
 
 =head1 SYNOPSIS
 
